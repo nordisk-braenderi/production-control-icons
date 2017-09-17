7f464d5c05037fc3565d09b664074aeb11a37cc9
Rasmus Nørskov <rasmus@rhnorskov.com>
2017-09-17 14:11:44 +0200

    update icons

---
+++ optimize.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+const svgo = new (require('svgo'))({
+  plugins: [
+    {removeDimensions: true},
+  ],
+});
+
+fs.readdir(path.resolve(__dirname, 'icons'), (err, files) => {
+  const svgs = files.filter(file => path.extname(file) === '.svg');
+
+  svgs.map(svg => {
+    const file = path.resolve(__dirname, 'icons', svg);
+    fs.readFile(file, 'utf8', (err, data) => {
+      // console.log(data);
+      svgo.optimize(data, ({data}) => {
+        fs.writeFile(file, data, err => {
+          console.log(err);
+        });
+      });
+    });
+  });
+
+});
